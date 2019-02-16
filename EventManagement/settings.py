f30eec0df8b646d312194bb0fff7eeb70ca535b5
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2019-02-16 23:20:45 +0530

    User module completed

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login_module',
 ]
+
+#authentication with usermodel
+AUTH_USER_MODEL = 'login_module.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
