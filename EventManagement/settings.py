50d64e6986d0e0a9eec83b38615d21a0736dd730
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2019-02-18 16:21:47 +0530

    Added Email System Configurations

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -125,6 +125,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='eventmanagementatict@gmail.com'
+EMAIL_HOST_PASSWORD ="Event@ict1718"
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
